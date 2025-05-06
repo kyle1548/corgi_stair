915087c369194742a158d2617388e6222e99c478
kyle1548 <kyle9296@gmail.com>
2025-05-06 17:09:05 +0800

    ..

--- src/normal_vector_plane_segmentation2.cpp
+++ src/normal_vector_plane_segmentation2.cpp
@@ -486,11 +486,16 @@ void cloudCallback(const sensor_msgs::PointCloud2ConstPtr& input) {
             if (distance >= lower && distance <= upper) {
                 int u = p.u;  // image column
                 // 若該 row 尚未記錄，或 z 值更大，就更新
-                double p_z = cluster_centroids[0].dot(p.position);
-                double pu_z = cluster_centroids[0].dot(row_max_z_map[u].position);
-                if (row_max_z_map.find(u) == row_max_z_map.end() || p_z > pu_z) {
-                    if (p.position.z() > plane_ranges[0][0].mean_distance + 0.05)
+                if (p.position.z() > plane_ranges[0][0].mean_distance + 0.05) {
+                    if (row_max_z_map.find(u) == row_max_z_map.end()) {
                         row_max_z_map[u] = p;
+                    } else {
+                        double p_z = cluster_centroids[0].dot(p.position);
+                        double pu_z = cluster_centroids[0].dot(row_max_z_map[u].position);
+                        if (p_z > pu_z) {
+                            row_max_z_map[u] = p;
+                        }
+                    }
                 }
             }
         }
