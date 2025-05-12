6f13d10ee42f669b5d6c8b8794dad6ade911f413
kyle1548 <kyle9296@gmail.com>
2025-05-13 06:32:47 +0800

    PCA for each plane point to get more precise normal

--- src/plane_segmentation.cpp
+++ src/plane_segmentation.cpp
@@ -231,6 +231,41 @@ std::vector<double> PlaneSegmentation::segment_by_distances(Eigen::Vector3f cent
         in_range = false;
     }//end if
 
+    std::vector<Eigen::Vector3f> points;
+    Eigen::Vector3f p_centroid(0, 0, 0);
+    if (mean_distances.size() >= 1 ) {
+        for (int i : indices) {
+            const pcl::Normal& n = normals_->points[i];
+            if (!std::isnan(n.normal_x) && !std::isnan(n.normal_y) && !std::isnan(n.normal_z)) {
+                Eigen::Vector3f position(cloud_->points[i].x,
+                                        cloud_->points[i].y,
+                                        cloud_->points[i].z);
+                double d = centroid.dot(position);  // projective distance
+                if (d > mean_distances[0]-bin_width && d < mean_distances[0]+bin_width) {
+                    p_centroid += position;
+                    points.push_back(position);
+                }
+            }//end if
+        }//end for
+    }
+
+    if (points.size() >= 1) {
+        p_centroid /= static_cast<double>(points.size());
+        std::cout << "p_centroid: " << p_centroid << std::endl;
+        Eigen::MatrixXf A(points.size(), 3);
+        for (size_t i = 0; i < points.size(); ++i) {
+            A.row(i) = points[i] - p_centroid;
+        }
+        Eigen::JacobiSVD<Eigen::MatrixXf> svd(A, Eigen::ComputeThinU | Eigen::ComputeThinV);
+        Eigen::Vector3f normal = svd.matrixV().col(2);  // 最小奇異值對應方向（即平面法向）
+        std::cout << "before normal: " << centroid << std::endl;
+        std::cout << "after  normal: " << normal << std::endl;
+        std::cout << "before d: " << mean_distances[0] << std::endl;
+        std::cout << "after  d: " << -normal.dot(p_centroid) << std::endl;
+
+    }
+
+    
     /* Only keep max bin in a range (merge_threshold) */
     std::vector<bool> keep(mean_distances.size(), true);
     for (size_t i = 0; i < mean_distances.size(); i++) {
