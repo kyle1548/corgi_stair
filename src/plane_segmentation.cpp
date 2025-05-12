df9ffe7ec8993442c9a6253ade642393896fcf15
kyle1548 <kyle9296@gmail.com>
2025-05-13 04:44:31 +0800

    .

--- src/plane_segmentation.cpp
+++ src/plane_segmentation.cpp
@@ -185,8 +185,7 @@ std::vector<double> PlaneSegmentation::segment_by_distances(Eigen::Vector3f cent
     for (double d : distances) {
         int bin = static_cast<int>((d - min_val) / bin_width);
         histogram[bin]++;
-        // bin_values[bin] += d;
-        bin_values[bin] = d;
+        bin_values[bin] += d;
     }//end for
     
     /* 找出連續高密度 bin */
@@ -218,7 +217,7 @@ std::vector<double> PlaneSegmentation::segment_by_distances(Eigen::Vector3f cent
             if (sum_count >= total_point_threshold) {
                 total_counts.push_back(sum_count);
                 // mean_distances.push_back(sum_value / sum_count);
-                mean_distances.push_back(bin_values[max_bin_index]);
+                mean_distances.push_back(bin_values[max_bin_index] / histogram[max_bin_index]);
             }//end if
             in_range = false;
         }//end if else
@@ -227,7 +226,7 @@ std::vector<double> PlaneSegmentation::segment_by_distances(Eigen::Vector3f cent
         if (sum_count >= total_point_threshold) {
             total_counts.push_back(sum_count);
             // mean_distances.push_back(sum_value / sum_count);
-            mean_distances.push_back(bin_values[max_bin_index]);
+            mean_distances.push_back(bin_values[max_bin_index] / histogram[max_bin_index]);
         }//end if
         in_range = false;
     }//end if
