0c23e101c407c79709d197835c87f020c3e7f76f
kyle1548 <kyle9296@gmail.com>
2025-06-12 22:02:22 +0800

    #include <fstream>

--- src/stair_main_vision.cpp
+++ src/stair_main_vision.cpp
@@ -11,6 +11,7 @@
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Matrix3x3.h>
+#include <fstream>
 
 #include "corgi_msgs/MotorCmdStamped.h"
 #include "corgi_msgs/TriggerStamped.h"
