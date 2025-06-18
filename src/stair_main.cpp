f248061b0e8ea6b9c73266f5c0c2050160c11b57
kyle1548 <kyle9296@gmail.com>
2025-06-19 04:23:35 +0800

    .

--- src/stair_main.cpp
+++ src/stair_main.cpp
@@ -152,7 +152,7 @@ int main(int argc, char** argv) {
                 // Adjust last step length of walk gait, foothold of last walk step should not exceed min_keep_stair_d.
                 // max_step_length_last = ((-D/2.0 - min_keep_stair_d) - hip_x) / (0.2+0.4); // step length if from current pos to min_keep_stair_d, step_length*(swing_phase + (1-swing_phase)/2) = foothold_x - hip_x
                 // max_step_length_last = (-D/2.0 - 0.20 - hip_x - 0.3*step_length)*5; // step length if from current pos to min_keep_stair_d, step_length*(swing_phase + (1-swing_phase)/2) = foothold_x - hip_x
-                max_step_length_last = (-D/2.0 - optimal_foothold - hip_x - 0.3*step_length_to_stair)*5; // step length if from current pos to min_keep_stair_d, step_length*(swing_phase + (1-swing_phase)/2) = foothold_x - hip_x
+                max_step_length_last = -D/2.0 - optimal_foothold - hip_x - 0.3*step_length_to_stair; // step length if from current pos to min_keep_stair_d, step_length*(swing_phase + (1-swing_phase)/2) = foothold_x - hip_x
                 min_steps = static_cast<int>(std::ceil(max_step_length_last / 0.3));   // max step length = 30cm
                 max_steps = static_cast<int>(std::floor(max_step_length_last / 0.2));  // min step length = 20cm
                 // std::cout << "max_step_length_last: " << max_step_length_last << std::endl;
