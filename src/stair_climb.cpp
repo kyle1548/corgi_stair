eb615cfffef71129f253fa64d2dada4daf65e7f2
kyle1548 <kyle9296@gmail.com>
2025-06-19 05:10:29 +0800

    no move if front leg not one_step for hind leg

--- src/stair_climb.cpp
+++ src/stair_climb.cpp
@@ -95,6 +95,9 @@ std::array<std::array<double, 4>, 2> StairClimb::step() {
     switch (this->state) {
         case MOVE_STABLE:
             if (last_state != state) {
+                if (swing_leg >= 2 && leg_info[swing_leg].next_up && !leg_info[other_side_leg[swing_leg][0]].one_step) {
+                    swing_count++;
+                }
                 init_move_CoM_stable(swing_sequence[swing_count % 4]);
             }//end if
             finish_move = move_CoM_stable();
@@ -984,36 +987,41 @@ bool StairClimb::determine_next_foothold() {
     if (!stair_edge[swing_leg].empty()) {
         current_stair_edge  = stair_edge[swing_leg].front().edge;
         if ((leg_info[swing_leg].next_up || leg_info[other_side_leg[swing_leg][1]].next_up) &&
-        (swing_leg < 2 || leg_info[other_side_leg[swing_leg][0]].one_step || leg_info[other_side_leg[swing_leg][0]].contact_edge || current_stair_count + 1 < leg_info[other_side_leg[swing_leg][0]].stair_count)) {
+        (swing_leg < 2 || leg_info[other_side_leg[swing_leg][0]].one_step || current_stair_count + 1 < leg_info[other_side_leg[swing_leg][0]].stair_count)) {
             leg_info[swing_leg].next_up = false;
             if (current_stair_count == leg_info[other_side_leg[swing_leg][1]].stair_count) {    //first swing leg 
                 leg_info[swing_leg].one_step = false;
                 leg_info[swing_leg].next_foothold = {current_stair_edge[0] + keep_edge_d, current_stair_edge[1]};
             } else {    //second swing leg: the same as first swing leg unless the leg can not reach keep_stair_d_max (because change first swing leg at each stair)
                 #if CHANGE_FIRST_SWING_LEG
-                if (swing_leg < 2) {    // front leg: change first swing leg
-                    double deepest_x = stair_edge[swing_leg][1].edge[0] - keep_stair_d_min;
+                // if (swing_leg < 2) {    // front leg: change first swing leg
+                if (true) {    // front leg: change first swing leg
+                    double deepest_x;
                     double min_deepest_x;
                     double next_max_foothold_x = leg_info[swing_leg].get_hip_position(CoM, pitch)[0] + step_length_up_stair / 2;
                     if (stair_edge[swing_leg].size() >= 2) {
                         // min_deepest_x = stair_edge[swing_leg][1].edge[0] - keep_stair_d_min - step_length_up_stair / 2;
+                        deepest_x = stair_edge[swing_leg][1].edge[0] - keep_stair_d_min;
                         double middle_foothold = (deepest_x + leg_info[other_side_leg[swing_leg][1]].foothold[0]) / 2;
                         min_deepest_x = std::min({middle_foothold, next_max_foothold_x});
                     } else {
                         min_deepest_x = INFINITY;
+                        min_deepest_x = next_max_foothold_x;
                     }//end if else
                     // double next_max_foothold_x = std::min({stair_edge[swing_leg][0].edge[0]+min_foothold_distance, leg_info[swing_leg].get_hip_position(CoM, pitch)[0] + step_length_up_stair / 2});
                     // if (next_max_foothold_x >= min_deepest_x) {
-                    if (min_deepest_x >= stair_edge[swing_leg].front().edge[0]+keep_edge_d+min_foothold_distance) {
-                        // leg_info[swing_leg].one_step = false;
-                        leg_info[swing_leg].one_step = true;
-                        // leg_info[swing_leg].next_foothold =  leg_info[other_side_leg[swing_leg][1]].foothold;
-                    } else {
-                        // leg_info[swing_leg].one_step = true;
-                        leg_info[swing_leg].one_step = false;
-                        // leg_info[swing_leg].next_foothold = {next_max_foothold_x, current_stair_edge[1]};
-                    }//end if else
+                    //     // leg_info[swing_leg].one_step = false;
+                    //     // leg_info[swing_leg].one_step = true;
+                    //     leg_info[swing_leg].next_foothold =  leg_info[other_side_leg[swing_leg][1]].foothold;
+                    // } else {
+                    //     // leg_info[swing_leg].one_step = true;
+                    //     // leg_info[swing_leg].one_step = false;
+                    //     leg_info[swing_leg].next_foothold = {next_max_foothold_x, current_stair_edge[1]};
+                    // }//end if else
                     leg_info[swing_leg].next_foothold = {min_deepest_x, current_stair_edge[1]};
+                    if (leg_info[swing_leg].next_foothold[0] >= stair_edge[swing_leg][0].edge[0]+min_foothold_distance) {
+                        leg_info[swing_leg].one_step = true;
+                    }
                 } else {    // hind leg: no change first swing leg
                     leg_info[swing_leg].one_step = true;
                     double deepest_x;
